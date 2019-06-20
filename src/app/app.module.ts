222e624110c4bd26a729400337603b800d52a1b3
espinozallas <espinoza.isd@gmail.com>
2019-06-20 16:40:22 -0700

    6 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginService }         from './login.service';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent }          from './app.component';
 import { LoginComponent } from './login/login.component';
 
-@NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: LoginComponent , pathMatch: 'full'},
-      { path: 'login', component: LoginComponent,  pathMatch: 'full'}
-    ])
-  ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent ],
-  bootstrap:    [ AppComponent ]
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit() {
+    
+    this.loginService.login('peter@klaven', 'cityslicka').subscribe(
+      res => {
+        console.log(res);      
+    });
+
+  }
+}
+
+const appRoutes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'login', component: LoginComponent},
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  declarations: [
+    AppComponent,
+  LoginComponent,
+  ],
+  bootstrap: [ AppComponent ]
+})
+
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,4 +12,28 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  logIn(username: string, password: string, event: Event) {
+    event.preventDefault(); // Avoid default action for the submit button of the login form
+
+    // Calls service to login user to the api rest
+    this.loginService.login(username, password).subscribe(
+
+      res => {
+       console.log(res);
+
+      },
+      error => {
+        console.error(error);
+        
+      },
+
+      () => this.navigate()
+    );
+
+  }
+
+  navigate() {
+    this.router.navigateByUrl('/home');
+  }
+
 }
