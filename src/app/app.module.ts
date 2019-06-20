b0715fbe8dc84e087bbdd5d2c0c0e38582059f61
espinozallas <espinoza.isd@gmail.com>
2019-06-20 15:50:31 -0700

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent , pathMatch: 'full'},
+      { path: 'login', component: LoginComponent,  pathMatch: 'full'}
+    ])
+  ],
   declarations: [ AppComponent, HelloComponent, LoginComponent ],
   bootstrap:    [ AppComponent ]
 })
